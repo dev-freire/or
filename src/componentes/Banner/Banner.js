daaff818af64c31c3954d89a59d082ef91a46f7f
dev-freire <dev.freire@gmail.com>
2024-02-20 16:42:14 -0300

    iniciando o projeto

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,16 @@
+import './Banner.css'
+
+function Banner() {
+    //JSX
+    return (
+        <header className="banner">
+            <img src="/img/banner.png" alt="o banner principal da página." />
+        </header>
+        
+
+    )
+
+
+}
+
+export default Banner
